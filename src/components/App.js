3469c82d4f2bbb7e5b89be87e0adff3eb362bf0f
Shrey <shreyshukla1010@gmail.com>
2024-10-13 18:12:48 +0530

    progress bar

--- src/components/App.js
+++ src/components/App.js
@@ -5,11 +5,15 @@ import Loader from "./Loader";
 import Error from "./Error";
 import StartScreen from "./StartScreen";
 import Question from "./Question";
+import NextButton from "./NextButton";
+import Progress from "./Progress";
 
 const initialState = {
   questions: [],
   status: "loading",
   index: 0,
+  answer:null,
+  points:0
 };
 
 function reducer(state = initialState, action) {
@@ -20,17 +24,30 @@ function reducer(state = initialState, action) {
       return { ...state, status: "error" };
     case "start":
       return { ...state, status: "active" };
+    case "newAnswer":
+      const question = state.questions.at(state.index)
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case "nextQuestion":
+      return {...state ,index :state.index+1,answer:null}
     default:
       throw new Error("Action Unknown");
   }
 }
 
 export default function App() {
-  const [{ questions, status, index }, dispatch] = useReducer(
+  const [{ questions, status, index,answer,points }, dispatch] = useReducer(
     reducer,
     initialState
   );
   const numQuestions = questions.length;
+  const maxPoints = questions.reduce((prev,curr)=> prev+curr.points ,0)
 
   useEffect(function () {
     fetch("http://localhost:8000/questions")
@@ -48,7 +65,24 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question question={questions[index]} />}
+
+        {status === "active" && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPoints={maxPoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              dispatch={dispatch}
+              answer={answer}
+            />
+            <NextButton dispatch={dispatch} answer={answer} />
+          </>
+        )}
       </Main>
     </div>
   );
--- src/components/Options.js
+++ src/components/Options.js
@@ -1,8 +1,17 @@
-function Options({question}){
+function Options({question, dispatch , answer }){
     return (
       <div className="options">
-        {question.options.map((option) => (
-          <button className="btn btn-option" key={option}>
+        {question.options.map((option, index) => (
+          <button
+            className={`btn btn-option ${index === answer ? "answer" : ""} ${
+              answer !== null ? index === question.correctOption
+                ? "correct"
+                : "wrong" : ""
+            }`}
+            key={option}
+            disabled={answer !== null}
+            onClick={() => dispatch({ type: "newAnswer", payload: index })}
+          >
             {option}
           </button>
         ))}
